19d44fa49962ae3986fcfc58a71135dd5a3ba231
Frothbeast <frothbeast@gmail.com>
2026-07-07 17:40:38 -0400

    changed colour of chart and size of setpoint font Signed-off-by: Frothbeast <frothbeast@gmail.com>

--- client/src/components/sidebar/heaterSidebar.js
+++ client/src/components/sidebar/heaterSidebar.js
@@ -55,9 +55,9 @@ const HeaterSidebar = ({ isOpen, records, selectedHours }) => {
           <HeaterChart
             labels={labels}
             datasets={[
-              { label: "Setpoint", color: "white", data: records.map(r => r.setpoint), borderWidth: 0.5, tension: 0 },
+              { label: "Setpoint", color: "green", data: records.map(r => r.setpoint), borderWidth: 0.5, tension: 0 },
               { label: "Box °C", color: "red", data: records.map(r => r.tempBox), borderWidth: 0.5, tension: 0 },
-              { label: "Heater °C", color: "pink", data: records.map(r => r.tempHeater), borderWidth: 0.5, tension: 0 }
+              { label: "Heater °C", color: "yellow", data: records.map(r => r.tempHeater), borderWidth: 0.5, tension: 0 }
             ]}
             options={config}
           />
